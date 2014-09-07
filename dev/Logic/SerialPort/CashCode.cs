0b381d7501417e9fc0346bef80dfed80f294cdb0
ram2406 <devnull@localhost>
2014-09-07 12:53:23 +0400

    cashcode complete

--- dev/Logic/SerialPort/CashCode.cs
+++ dev/Logic/SerialPort/CashCode.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Logic.SerialPort
 {
-    public class CashCode
+    public abstract class ACashCode
     {
+  
 
     }
+   
 }
--- dev/OriflameApp/MainWindow.xaml.cs
+++ dev/OriflameApp/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Logic.SerialPort;
+//using CP = CSJWPOS;
+using Microsoft.PointOfService;
+using System.Diagnostics;
 
 namespace OriflameApp
 {
@@ -24,5 +28,95 @@ namespace OriflameApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            CashCode cc= new CashCode();
+            cc.NumberOfComPort = ushort.Parse(txb.Text);
+            cc.OpenComPort();
+           
+            cc.DelegatePollingBill = PollingBill;
+            cc.DelegateProcessMessage = ProcessMessage;
+            cc.Reset();
+            
+            string name = string.Empty, id = string.Empty;
+            cc.Identification(ref name, ref id);
+
+            l_id.Content = name + " " + id;
+
+            cc.Reset();
+            var rubs = Enum.GetValues(typeof(CashCode.RUB));
+            var lrubs = rubs.OfType<CashCode.RUB>().ToList();
+            cc.EnableBillTypes(new List<CashCode.RUB> { CashCode.RUB.B100});
+            
+            var res = cc.PollingLoop(100, 20);
+            cc.Reset();
+
+            cc.CloseComPort();
+        }
+        void PollingBill(UInt16 nominal, ref bool canLoop)
+        {
+            lst.Items.Add(nominal);
+        }
+        void ProcessMessage(uint error, string message)
+        {
+            lst.Items.Add(error + message);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var myExplorer = new PosExplorer();
+            var devices = myExplorer.GetDevices("PosPrinter");
+            var device = myExplorer.GetDevice("PosPrinter", "CT-S2000_1"); 
+            var inst = myExplorer.CreateInstance(device);
+            PosPrinter printer = (PosPrinter)inst;
+            printer.OutputCompleteEvent += printer_OutputCompleteEvent;
+            printer.Open();
+            printer.Claim(1000);
+            printer.DeviceEnabled = true;
+            
+            
+            
+            printer.PrintNormal(PrinterStation.Receipt, "test");
+            //printer.EndInsertion();
+            printer.DirectIO(
+            printer.TransactionPrint(PrinterStation.Receipt, PrinterTransactionControl.Normal);
+            printer.DeviceEnabled = false;
+            printer.Release();
+            printer.Close();
+            //PosPrinter printer = (Pos) inst;
+            //printer.PrintNormal(PrinterStation.None, "test");
+            //DeviceInfo device = myExplorer.GetDevice("Msr", "MGTK_MSR");
+           /*CP.CTCashDrawer2 drawer = (CP.CTCashDrawer2)inst;
+            drawer.Claim(1000);
+            drawer.Open();
+            */
+            /*
+            CP.CTCashDrawer2 cc = new CP.CTCashDrawer2();
+            cc.Open();
+            
+            try
+            {
+                var printer = new CP.CTS2000POSPrinter();
+                printer.Open();
+                printer.Claim(1000);
+                printer.DeviceEnabled = true;
+                printer.PrintNormal(Microsoft.PointOfService.PrinterStation.None, "test");    
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            */
+           
+         
+            
+        }
+
+        void printer_OutputCompleteEvent(object sender, OutputCompleteEventArgs e)
+        {
+            Debug.Assert(false);
+        }
     }
+    
 }
